2b2e5b5fb41e673ed166e43033f3c22da1879d8d
Cary Sun <mingruisun@hotmail.com>
2019-04-26 10:31:43 +0800

    Initial Commit

---
+++ EggTimer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  EggTimer
+//
+//  Created by Cary Sun on 4/26/19.
+//  Copyright © 2019 Cary Sun. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ EggTimer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by Cary Sun on 4/26/19.
+//  Copyright © 2019 Cary Sun. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
