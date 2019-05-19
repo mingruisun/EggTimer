5f69aa5adf343ca705a5eda451b27ef472d3047a
Cary Sun <carysun@Carys-iMac-Pro.local>
2019-05-19 15:31:26 -0700

    added Preferences window

---
+++ EggTimer/PrefsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PrefsViewController.swift
+//  EggTimer
+//
+//  Created by Cary Sun on 5/19/19.
+//  Copyright © 2019 Cary Sun. All rights reserved.
+//
+
+import Cocoa
+
+class PrefsViewController: NSViewController {
+
+    @IBOutlet weak var presetsPopup: NSPopUpButton!
+    @IBOutlet weak var customTextField: NSTextField!
+    @IBOutlet weak var customSlider: NSSlider!
+    
+    @IBAction func popupValueChanged(_ sender: Any) {
+    }
+    @IBAction func sliderValueChanged(_ sender: Any) {
+    }
+    @IBAction func cancelButtonClicked(_ sender: Any) {
+    }
+    @IBAction func okButtonClicked(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
